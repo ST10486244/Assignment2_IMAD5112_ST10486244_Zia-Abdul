ed9d46dc4331011bdf2ca574132ab919fd48ffe7
Zia Abdul <ST10486244@vcconnect.edu.za>
2025-05-19 22:40:41 +0200

    Third commit - Code update

--- app/src/main/java/za/co/varsitycollege/assignment2_imad5112_st10486244_zia_abdul/Scoresheet.kt
+++ app/src/main/java/za/co/varsitycollege/assignment2_imad5112_st10486244_zia_abdul/Scoresheet.kt
@@ -1,7 +1,9 @@
 package za.co.varsitycollege.assignment2_imad5112_st10486244_zia_abdul
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,10 +17,13 @@ class Scoresheet : AppCompatActivity() {
         val score = intent.getIntExtra("SCORE", 0)
         val total = intent.getIntExtra("TOTAL", 0)
         val results = intent.getStringArrayListExtra("RESULTS")
+        val correctAnswers = intent.getStringArrayListExtra("CORRECT_ANSWERS")
 
         // Get the TextViews
         val scoreTextView = findViewById<TextView>(R.id.scoreTextView)
         val resultTextView = findViewById<TextView>(R.id.totalTextView)
+        val exitButton = findViewById<Button>(R.id.exitButton)
+
 
         // Set the score
         scoreTextView.text = "You scored $score out of $total"
